1914263d654f5dcf7799f63fbc8fef92e7c56b52
Zaheer khan,Zaheer Khan, Habib khan, Habib Khan <zaheerkhan872004@gmail.com,zaheer.224246108@vcet.edu.in,habibkhan15031999@gmail.com,habibkhan19992022@gmail.com>
2024-01-23 20:29:55 +0530

    partially controller completed

---
+++ src/controllers/city-controller.js
@@ -0,0 +1,48 @@
+const { CityService, CityService } = require('../services/index');
+
+const cityService = new CityService();
+
+/**
+ * 
+ * POST method used here
+ * data -> req.body 
+ */
+const create = (req,res) => {
+    try {
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const destroy = (req,res) => {
+    try {
+        
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+const get = (req,res) => {
+    try {
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const update = (req,res) => {
+    try {
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+module.exports = {
+    create,
+    destroy,
+    get,
+    update
+}
